4c50f0486a5c02a62d442cbe3e6130a4dd03b3c6
arshilkhann__ <arshilkhan529@gmail.com>
2025-05-19 18:47:27 +0530

    Add files via upload
    This is my first Challenge of Java

---
+++ Multiplication.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        System.out.print("Enter the number for multiplication Table : ");
+        int num = number.nextInt();
+        int result =multi(num);
+    }
+    public static int multi(int num){
+        int i = 1 ;
+        while (i <= 10){
+            System.out.println(num+"x"+i+"="+num*i);
+            i++ ;
+        }
+        return i;
+    }
+}
