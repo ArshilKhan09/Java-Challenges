4ad88d22b1ebe177d88e3ca26c656cbeb978b6c8
arshilkhann__ <arshilkhan529@gmail.com>
2025-05-22 22:10:13 +0530

    Add files via upload
    This is my Eighth Problem
    This Program is about to reverse the digit of a number

---
+++ reverse.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class reverse {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = inp.nextInt();
+        int reverse = reverse(num);
+        System.out.println("The reverse of the number is : "+reverse);
+    }
+    public static int reverse(int num){
+        int sum = 0 ;
+        while (num > 0){
+            int digit = num % 10 ;
+            sum = sum * 10 + digit;
+            num /= 10 ;
+        }
+        return sum ;
+    }
+}
