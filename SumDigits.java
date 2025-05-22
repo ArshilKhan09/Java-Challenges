83956ec3e9a945aaa550928084c191f36f56f18e
arshilkhann__ <arshilkhan529@gmail.com>
2025-05-22 19:50:57 +0530

    Add files via upload
    This is my third program
    This program is  about to add all the digits in the number

---
+++ SumDigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumDigits {
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = number.nextInt();
+        int digits = sumOfDigit(num);
+        System.out.println("The sum of "+num+" is : "+digits);
+    }
+    public static int sumOfDigit(int num){
+        int sum = 0 ;
+        while (num > 0){
+            sum += num % 10 ;
+            num /= 10 ;
+        }
+        return sum ;
+    }
+}
