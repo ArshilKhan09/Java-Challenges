755aa17b80d359978fda1ce776697f371bf45184
arshilkhann__ <arshilkhan529@gmail.com>
2025-05-22 19:42:11 +0530

    Add files via upload
    This is my Second problem 
    The problem is about to add all the integers in a number

---
+++ SumOdd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumOdd {
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = number.nextInt();
+        int sum =sumOfOdd(num);
+        System.out.println("The sum of all odd number till "+num+" is : "+sum);
+    }
+
+    public static int sumOfOdd(int num){
+        int sum = 0 ;
+        int i = 1 ;
+        while (i <= num) {
+            sum += i ;
+            i += 2 ;
+        }
+        return sum;
+    }
+}
