bb8ed75c8ee0b56978d7430d7ea445980f15355c
arshilkhann__ <arshilkhan529@gmail.com>
2025-05-22 21:57:50 +0530

    Add files via upload
    This is my Seventh Problem
    This program is about to show the output of the entered number is Prime or Not

---
+++ prime.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class prime {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = inp.nextInt();
+        boolean isPrime = isprime(num);
+        if (isPrime){
+            System.out.println("The number is prime");
+        }
+        else {
+            System.out.println("The number is not Prime");
+        }
+    }
+    public static boolean isprime(int num){
+          int i = 2 ;
+          while (i < num){
+          if(num % i == 0){
+              return false;
+          }
+              i++;
+          }
+             return true ;
+    }
+}
