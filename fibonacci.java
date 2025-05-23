8712c5785586f7bac5129d03ad1248432da4d020
arshilkhann__ <arshilkhan529@gmail.com>
2025-05-23 18:57:24 +0530

    Add files via upload
    This is my Ninth Problem
    This Program is about to print the fibonacci series upto "n" Number

---
+++ fibonacci.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = number.nextInt();
+        System.out.println("The fibonacci series of "+num+" is :");
+        fibonacci(num);
+    }
+    public static void fibonacci(int num){
+       if (num < 0) return;
+        System.out.print("0 ");
+       if (num == 0) return;
+        System.out.print("1 ");
+
+        int first = 0 , second = 1 ;
+       while (first+second <= num){
+           int third = first+second ;
+           System.out.print(third+" ");
+           first = second ;
+           second = third;
+       }
+    }
+}
