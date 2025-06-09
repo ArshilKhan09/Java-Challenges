9d7297601dcc7521772976d3490fbe6660901447
ArshilKhan09 <arshilkhan529@gmail.com>
2025-06-09 23:29:36 +0530

    Enhanced the code

--- Occurence.java
+++ Occurence.java
@@ -4,22 +4,18 @@ public class Occurence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input array size
         System.out.print("Enter the number of elements in the array: ");
         int n = scanner.nextInt();
 
-        // Create and fill the array
         int[] newArray = new int[n];
         System.out.println("Enter " + n + " integers:");
         for (int i = 0; i < n; i++) {
             newArray[i] = scanner.nextInt();
         }
 
-        // Input number to find
         System.out.print("Enter the number to find the occurrence: ");
         int num = scanner.nextInt();
 
-        // Calculate occurrence
         int occurance = occurance(newArray, num);
         System.out.println("The occurrence of the given number is: " + occurance);
 
