1369281d1eeb57b8759ffd565f619853f6581946
ArshilKhan09 <arshilkhan529@gmail.com>
2025-06-22 19:24:59 +0530

    Added New File

---
+++ classMath.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class classMath {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the side A :");
+        double sideA = input.nextDouble();
+        System.out.print("Enter the side B :");
+        double sideB = input.nextDouble();
+
+        double hypotenuse_c = Math.sqrt(Math.pow(sideA,2) + Math.pow(sideB,2));
+        System.out.printf("The Hypotenuse Side C is : %.2fcm\n",hypotenuse_c);
+
+        input.close();
+    }
+}
