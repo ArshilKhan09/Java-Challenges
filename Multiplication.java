0bd776b6af95309b858a7a1068f182b789865c8a
ArshilKhan09 <arshilkhan529@gmail.com>
2025-07-22 19:03:17 +0530

    Updated The File

--- Multiplication.java
+++ Multiplication.java
@@ -2,32 +2,26 @@ import java.util.Scanner;
 
 public class Multiplication {
     public static void main(String[] args) {
-        Scanner number = new Scanner(System.in); // Create Scanner object for user input
+        Scanner number = new Scanner(System.in); 
 
-        // Ask the user to enter a number for which the multiplication table will be printed
         System.out.print("Enter the number for multiplication Table: ");
-        int num = number.nextInt(); // Read the input number
+        int num = number.nextInt(); /
 
-        // Call the multi() method to print the multiplication table
         int result = multi(num);
 
-        // Print the final value returned by multi() (which is not very useful in this case)
-        System.out.println(result); // This will just print 11 (you can remove this if not needed)
+        System.out.println(result); 
 
-        number.close(); // Close the Scanner
+        number.close();
     }
 
-    // Method to print the multiplication table of a given number
     public static int multi(int num) {
         int i = 1;
 
-        // Loop from 1 to 10 to print the multiplication table
         while (i <= 10) {
-            // Print one line of the table: num x i = result
             System.out.println(num + " x " + i + " = " + (num * i));
-            i++; // Move to the next multiplier
+            i++; 
         }
 
-        return i; // This returns 11, but it's not really needed
+        return i; 
     }
 }
