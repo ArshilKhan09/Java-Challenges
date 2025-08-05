d39d6d9b615addb079aa99e46ae647063b02e11a
ArshilKhan09 <arshilkhan529@gmail.com>
2025-08-05 16:23:41 +0530

    Added New File

--- Wrapper.java
+++ Wrapper.java
@@ -1,3 +1,16 @@
 public class Wrapper {
-    
+   
+    public static void main(String[] args) {
+        int a = 10;  // primitive data type
+
+        // Wrapping primitive into object (Boxing)
+        Integer obj = Integer.valueOf(a);  
+
+        // Unwrapping object to primitive (Unboxing)
+        int b = obj.intValue();  
+
+        System.out.println("Primitive value: " + a);
+        System.out.println("Wrapped object: " + obj);
+        System.out.println("Unwrapped value: " + b);
+    }
 }
