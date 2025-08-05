748d465e6b5b248f27fbc42e826eecade9947f41
ArshilKhan09 <arshilkhan529@gmail.com>
2025-08-05 17:58:03 +0530

    Addded New File

---
+++ pack1/course.java
@@ -0,0 +1,21 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class course {
+        public String courseName;
+    public int courseCode;
+
+    public void readCourse() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Course Name: ");
+        courseName = sc.nextLine();
+        sc.nextLine();
+        sc.close();
+    }
+
+    public void displayCourse() {
+        System.out.println("Course Name: " + courseName);
+    }
+
+}
---
+++ pack1/student.java
@@ -0,0 +1,23 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class student {
+     public String name;
+    public int rollNo;
+
+    public void readStudent() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Student Name: ");
+        name = sc.nextLine();
+        System.out.print("Enter Roll Number: ");
+        rollNo = sc.nextInt();
+        sc.nextLine();
+        sc.close();
+    }
+
+    public void displayStudent() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll Number: " + rollNo); 
+    }
+}
