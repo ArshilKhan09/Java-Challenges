fec34649375b02134aa29e6c283dd8c02b6e7383
ArshilKhan09 <arshilkhan529@gmail.com>
2025-08-07 21:26:40 +0530

    Added New File

--- STATIC.java
+++ STATIC.java
@@ -1,3 +1,15 @@
-public class STATIC {
-    
+import java.util.Scanner;
+
+public class STATIC {  
+    public static void main(String[] args) {
+        printNumber();
+    }      
+
+    public static void printNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int input = sc.nextInt();
+        System.out.println(input);
+        sc.close();
+    }
 }
