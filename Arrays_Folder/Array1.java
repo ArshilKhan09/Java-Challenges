e7a90148728174736d7a7f5aab533c08e2f60512
ArshilKhan09 <arshilkhan529@gmail.com>
2025-12-11 20:56:21 +0530

    Array Example

--- Arrays_Folder/Array1.java
+++
@@ -1,9 +0,0 @@
-public class Array1 {
-
-    // Syntax For creating an Array 
-    int[] arr1 = new int[10]; // here 10 is the size of the Array [Used When we don't know the value]
-
-    int[] arr2 = {23,4,2,64,76,34,65,2,5,3}; // directly putting values without declaring the size [Used When we know the value]
-
-    
-}
