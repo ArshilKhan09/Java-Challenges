868ef2c610a2d5a239cebcb585b679a02742d23d
ArshilKhan09 <arshilkhan529@gmail.com>
2025-12-12 22:39:23 +0530

    Full Code

--- Arrays_Folder/Pallindrome.java
+++ Arrays_Folder/Pallindrome.java
@@ -1,23 +1,34 @@
+import java.util.Scanner;
+
 public class Pallindrome {
-    public boolean checkIfPalindrome(String s) {
+    public static boolean checkIfPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
 
         while (left < right) {
             if (s.charAt(left) != s.charAt(right)) {
+                System.out.println("Not Pallindrome");
                 return false;
             }
 
             left++;
             right--;
         }
-
+        System.out.println("Pallindrome");
         return true;
     }
 
 
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in) ;
+
+        System.out.print("Enter The String That you want to Check : ");
+        String s = sc.next();
+
+        checkIfPalindrome(s);
+
+        sc.close();
     }
 
 }
