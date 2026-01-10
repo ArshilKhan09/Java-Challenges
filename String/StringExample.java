34cf29dd801d07e1d4663124bcf9c76d61028039
ArshilKhan09 <arshilkhan529@gmail.com>
2026-01-10 22:13:23 +0530

    Added new String Folder For Understanding String

---
+++ String/StringExample.java
@@ -0,0 +1,8 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String name = "Arshil";
+        String myName = "Arshil";
+        name = "Khan";
+        System.out.println(name);
+    }
+}
