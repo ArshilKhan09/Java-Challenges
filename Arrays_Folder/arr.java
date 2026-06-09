e5e939f1df0f4a15aad0da90eed8d372d3ad8ac8
ArshilKhan09 <arshilkhan529@gmail.com>
2026-06-09 13:18:31 +0530

    Added two new file arr.java and arr.class in Arrays_Folder Directory

---
+++ Arrays_Folder/arr.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class arr {
+    public static void main(String[] args) {
+        int[] arr;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        int size = sc.nextInt();
+        arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = i+1;
+        }
+        System.out.println("The elements of the array are:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
