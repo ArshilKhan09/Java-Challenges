87b5452965ab018e8cf14d10a4f9bd1f7f4c4ba3
ArshilKhan09 <arshilkhan529@gmail.com>
2026-06-09 19:46:51 +0530

    Added display method for diplaying array elements

--- Arrays_Folder/arr.java
+++ Arrays_Folder/arr.java
@@ -4,18 +4,27 @@ public class arr {
     public static void main(String[] args) {
         int[] arr;
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter the size of the array : ");
         int size = sc.nextInt();
-        arr = new int[size];
+
+         arr = new int[size];
 
         for (int i = 0; i < size; i++) {
             arr[i] = i+1;
         }
+
+
+        display(arr, size);
+
+        sc.close();
+    }
+
+
+    static void display(int[] arr, int size) {
         System.out.println("The elements of the array are:");
         for (int i = 0; i < size; i++) {
             System.out.print(arr[i] + " ");
         }
-
-        sc.close();
     }
 }
