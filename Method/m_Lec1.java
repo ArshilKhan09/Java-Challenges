c4a66f63795489d3e078bbc282b4c8e65f5214b3
ArshilKhan09 <arshilkhan529@gmail.com>
2026-07-23 18:24:15 +0530

    Add adition() for understanding Methods

--- Method/m_Lec1.java
+++ Method/m_Lec1.java
@@ -1,4 +1,22 @@
 package Method;
 
+import java.util.Scanner;
+
 public class m_Lec1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first Number : ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter the second Number : ");
+        int b = sc.nextInt();
+
+
+    }
+
+
+    int addition(int x , int y){
+
+    }
 }
